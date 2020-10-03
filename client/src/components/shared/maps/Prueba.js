80dc79154836af66bf5a5fc99b5c7855187e6e57
diegomolina83 <diegomolina83@gmail.com>
2020-10-03 13:51:43 +0200

    email fix

---
+++ client/src/components/shared/maps/Prueba.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+
+class Prueba extends Component {
+    
+
+    render() {
+        return (
+           <h1>HOLA</h1>
+        );
+    }
+}
+
+export default Prueba;
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -10,6 +10,7 @@ router.post('/signup', (req, res, next) => {
 
     const username = req.body.username;
     const password = req.body.password;
+    const email = req.body.email;
 
     if (!username || !password) {
         res.status(400).json({ message: 'Empty fields' });
@@ -38,11 +39,13 @@ router.post('/signup', (req, res, next) => {
 
         const aNewUser = new User({
             username: username,
-            password: hashPass
+            password: hashPass,
+            email: email
         });
 
         aNewUser.save(err => {
             if (err) {
+                console.log(err)
                 res.status(500).json({ message: 'Error saving user to DB' });
                 return;
             }
