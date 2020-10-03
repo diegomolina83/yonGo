19b4834b18502c85dec35581a22dcc2b5a435db9
diegomolina83 <diegomolina83@gmail.com>
2020-10-03 13:06:37 +0200

    0.1s2

--- server/configs/mongoose.config.js
+++ server/configs/mongoose.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const connectionString = `mongodb://localhost/${process.env.DB_LOCAL}`
-
+// const connectionString = `mongodb://localhost/${process.env.DB_LOCAL}`
+const connectionString = `mongodb+srv://Diego:yonGo2020@cluster0.s6uo2.mongodb.net/test?authSource=admin&replicaSet=atlas-ma3x94-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`
 mongoose
     .connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
