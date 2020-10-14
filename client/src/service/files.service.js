13caab265c016935162a51771ff2437c405ff973
aretius <jonarechalde@gmail.com>
2020-10-14 12:52:44 +0200

    Server pointing for deploy

--- client/src/service/files.service.js
+++ client/src/service/files.service.js
@@ -4,7 +4,7 @@ export default class CoasterService {
 
     constructor() {
         this.api = axios.create({
-            baseURL: 'http://localhost:5000/api/files',
+            baseURL: process.env.REACT_APP_API_URL,
             withCredentials: true
         })
     }
