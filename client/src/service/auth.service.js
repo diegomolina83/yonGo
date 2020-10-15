a9997af0ef8002f5a8d70d5b6d3fbbae61c27196
aretius <jonarechalde@gmail.com>
2020-10-15 18:06:41 +0200

    Deploy 3 to heroku

--- client/src/service/auth.service.js
+++ client/src/service/auth.service.js
@@ -4,8 +4,8 @@ export default class AuthService {
 
     constructor() {
         this.api = axios.create({
-            baseURL: 'http://localhost:5000/api',
-            // baseURL: process.env.REACT_APP_API_URL
+            //baseURL: 'http://localhost:5000/api',
+            baseURL: process.env.REACT_APP_API_URL,
             withCredentials: true
         })
     }
--- client/src/service/files.service.js
+++ client/src/service/files.service.js
@@ -4,8 +4,8 @@ export default class CoasterService {
 
     constructor() {
         this.api = axios.create({
-            baseURL: 'http://localhost:5000/api/files',
-            // baseURL: process.env.REACT_APP_API_URL
+            //baseURL: 'http://localhost:5000/api/files',
+            baseURL: process.env.REACT_APP_API_URL,
             withCredentials: true
         })
     }
