6adf58b8d2ae453707e82fd7efa33a583ad4b16d
aretius <jonarechalde@gmail.com>
2020-10-15 22:21:45 +0200

    Deploy 14 to heroku

--- server/configs/middleware.config.js
+++ server/configs/middleware.config.js
@@ -4,7 +4,7 @@ const logger = require('morgan')
 
 const cors = require('cors')
 
-const whitelist = [process.env.DOMAIN_REMOTE, process.env.DOMAIN]
+const whitelist = [process.env.DOMAIN_REMOTE, process.env.DOMAIN, 'https://yongoclient.herokuapp.com']
 const corsOptions = {
     origin: (origin, cb) => {
         const originIsWhitelisted = whitelist.includes(origin)
