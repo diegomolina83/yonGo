46b928d2507a24e02dee25b6e48ae88a7e7b1823
aretius <jonarechalde@gmail.com>
2020-10-15 21:34:27 +0200

    Deploy 11 to heroku

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
     //const ENDPOINT = 'localhost:5000'
-    const ENDPOINT = 'https://yongoserver.herokuapp.com'
+    const ENDPOINT = 'https://yongoserver.herokuapp.com:5000'
 
     // console.log("LOCATION",location)
 
