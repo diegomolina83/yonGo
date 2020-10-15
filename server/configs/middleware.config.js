fefaf768ed89db99d51b9e8fd0ac05aaa367c519
aretius <jonarechalde@gmail.com>
2020-10-15 20:20:43 +0200

    Deploy 7 to heroku

--- server/configs/middleware.config.js
+++ server/configs/middleware.config.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const whitelist = [process.env.DOMAIN_REMOTE, process.env.DOMAIN]
 const corsOptions = {
     origin: (origin, cb) => {
-        const originIsWhitelisted = true
+        const originIsWhitelisted = whitelist.includes(origin)
         cb(null, originIsWhitelisted)
     },
     credentials: true
